15d05bb44820f1b021a053cacc3691605602aaf8
HanJiaLiang-test <125648813+HanJiaLiang-test@users.noreply.github.com>
2023-02-20 16:29:41 +0800

    Update jenkins_test.py

--- jenkins_test.py
+++ jenkins_test.py
@@ -9,7 +9,19 @@ print(1/0)
 a = "hahh"
 print(a + 2)
 
+# bug_3
 narg=len(sys.argv)
 if narg == 1:
     print('@Usage: input_filename nelements nintervals')
     break
+
+# bug_4
+def adder(n):
+     num = 0
+     while num < n:
+         yield num
+         num += 1
+     return num  #Noncompliant
+
+# bug_5
+exec 'print 1' # Noncompliant
